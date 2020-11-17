b9fcefa9318b2dcf44c3f9f6f45235cd3c4ce3b2
PHATTHARANIT TESJAROEN <63010727@KMITL.AC.TH>
2020-11-18 01:21:37 +0700

    a

--- cf171163/cf171163.cpp
+++ cf171163/cf171163.cpp
@@ -2,7 +2,7 @@
 #include<stdio.h>
 struct input {
 	int x[2];
-}A1, A2,A3;
+}A1, A2, A3;
 
 void ck() {
 	A3.x[1] = A1.x[1] + A1.x[2];
@@ -10,23 +10,23 @@ void ck() {
 	printf("A1 = %d\n", A3.x[1]);
 	printf("A2 = %d", A3.x[2]);
 	if (A3.x[1] < 0 && A3.x[2] < 0) {
-		printf("\nab = +");
+		printf("\nA1A2 = +");
 
 	}
 	if (A3.x[1] == 0 || A3.x[2] == 0) {
-		printf("\nab = 0");
+		printf("\nA1A2 = 0");
 
 	}
 	if ((A3.x[1] > 0 && A3.x[2] < 0) || (A3.x[1] < 0 && A3.x[2] > 0)) {
-		printf("\nab = -");
+		printf("\nA1A2 = -");
 	}
 	if (A3.x[1] > 0 && A3.x[2] > 0) {
-		printf("\nab = +");
+		printf("\nA1A2 = +");
 
 	}
 }
 int main() {
-	
+
 	printf("Input A1 (2) : ");
 	scanf("%d %d", &A1.x[1], &A1.x[2]);
 	printf("Input A2 (2) : ");
