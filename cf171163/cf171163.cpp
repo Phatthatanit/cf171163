12259c6abbd8c95f637ff3db05cb6f7370d2a258
PHATTHARANIT TESJAROEN <63010727@KMITL.AC.TH>
2020-11-18 01:17:28 +0700

    a

--- cf171163/cf171163.cpp
+++ cf171163/cf171163.cpp
@@ -1,33 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+struct input {
+	int x[2];
+}A1, A2,A3;
 
-int main() {
-	struct input {
-		int x, y;
-	}A1,A2;
-	printf("Input A1 (2) : ");
-	scanf("%d %d", &A1.x, &A1.y);
-	printf("Input A2 (2) : ");
-	scanf("%d %d", &A2.x, &A2.y);
-	int a = A1.x + A1.y;
-	int b = A2.x + A2.y;
-	printf("A1 = %d\n",a);
-	printf("A2 = %d", b);
-	if (a < 0 && b < 0) {
-		printf("\nab = +"); 
-	
-	}
-	if (a == 0 || b == 0) {
-		printf("\nab = 0");
-
-	}
-	if((a > 0 && b < 0)|| (a < 0 && b > 0)) {
-		printf("\nab = -");
-	}
-	if (a > 0 && b > 0) {
+void ck() {
+	A3.x[1] = A1.x[1] + A1.x[2];
+	A3.x[2] = A2.x[1] + A2.x[2];
+	printf("A1 = %d\n", A3.x[1]);
+	printf("A2 = %d", A3.x[2]);
+	if (A3.x[1] < 0 && A3.x[2] < 0) {
 		printf("\nab = +");
 
 	}
+	if (A3.x[1] == 0 || A3.x[2] == 0) {
+		printf("\nab = 0");
+
+	}
+	if ((A3.x[1] > 0 && A3.x[2] < 0) || (A3.x[1] < 0 && A3.x[2] > 0)) {
+		printf("\nab = -");
+	}
+	if (A3.x[1] > 0 && A3.x[2] > 0) {
+		printf("\nab = +");
+
+	}
+}
+int main() {
+	
+	printf("Input A1 (2) : ");
+	scanf("%d %d", &A1.x[1], &A1.x[2]);
+	printf("Input A2 (2) : ");
+	scanf("%d %d", &A2.x[1], &A2.x[2]);
+	ck();
 
 	return 0;
 }
